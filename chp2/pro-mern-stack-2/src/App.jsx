aa73a87c7b24c907764425a5fed6c751350ef912
chiaweilin <lin.chiawe@husky.neu.edu>
2020-05-04 14:33:28 -0700

    finish PASSING DATA USING PROPERTIES section

--- chp2/pro-mern-stack-2/src/App.jsx
+++ chp2/pro-mern-stack-2/src/App.jsx
@@ -3,11 +3,33 @@ class IssueFilter extends React.Component{
     return <div>This is a placeholder for the issue filter.</div>;
   }
 }
-class IssueTable extends React.Component{
-  render(){
-    return <div>This is a placeholder for a table of issues.</div>;
-  }
+class IssueTable extends React.Component {
+  render() {
+    const rowStyle = {border: "1px solid silver", padding: 4};
+    return (
+      <table style={{borderCollapse: "collapse"}}>
+        <thead>
+          <tr>
+            <th style={rowStyle}>ID</th>
+            <th style={rowStyle}>Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          <IssueRow rowStyle={rowStyle} issue_id={1} issue_title = "Error in console when clicking Add"/> {/* somehow pass Issue 1 data to this */}
+          <IssueRow rowStyle={rowStyle} issue_id={2} issue_title = "Missing bottom border on panel"/> {/* somehow pass Issue 2 data to this */}
+        </tbody>
+      </table>
+    ); }
 }
+class IssueRow extends React.Component {
+  render() {
+    const style = this.props.rowStyle;
+    return (
+      <tr>
+        <td style={style}>{this.props.issue_id}</td>
+        <td style={style}>{this.props.issue_title}</td>
+      </tr> );
+} }
 class IssueAdd extends React.Component{
   render(){
     return <div>This is a placeholder for a form to add an issue.</div>;
