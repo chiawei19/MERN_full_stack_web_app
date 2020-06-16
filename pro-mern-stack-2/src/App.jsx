429dc2094654d912ac261debbef8189e51a3d68b
chiaweilin <lin.chiawe@husky.neu.edu>
2020-06-16 02:14:42 -0700

    finish chapter 5

--- pro-mern-stack-2/src/App.jsx
+++ pro-mern-stack-2/src/App.jsx
@@ -81,6 +81,30 @@ class IssueAdd extends React.Component {
   }
 }
 
+async function graphQLFetch(query, variables = {}) {
+  try {
+    const response = await fetch('/graphql', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({ query, variables })
+    });
+    const body = await response.text();
+    const result = JSON.parse(body, jsonDateReviver);
+    if (result.errors) {
+      const error = result.errors[0];
+      if (error.extensions.code == 'BAD_USER_INPUT') {
+        const details = error.extensions.exception.errors.join('\n ');
+        alert(`${error.message}:\n ${details}`);
+      } else {
+        alert(`${error.extensions.code}: ${error.message}`);
+      }
+    }
+    return result.data;
+  } catch (e) {
+    alert(`Error in sending data to server: ${e.message}`);
+  }
+}
+
 class IssueList extends React.Component {
   constructor() {
     super();
@@ -99,15 +123,10 @@ class IssueList extends React.Component {
         created effort due
       }
     }`;
-
-    const response = await fetch('/graphql', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json'},
-      body: JSON.stringify({ query })
-    });
-    const body = await response.text();
-    const result = JSON.parse(body, jsonDateReviver);
-    this.setState({ issues: result.data.issueList });
+    const data = await graphQLFetch(query);
+    if (data) {
+          this.setState({ issues: data.issueList });
+        }
   }
 
   async createIssue(issue) {
@@ -117,12 +136,10 @@ class IssueList extends React.Component {
       }
     }`;
 
-    const response = await fetch('/graphql', {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({query, variables: { issue }})
-     });
-    this.loadData();
+    const data = await graphQLFetch(query, { issue });
+    if (data) {
+      this.loadData();
+    }
   }
 
   render() {
