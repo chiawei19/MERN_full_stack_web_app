f8f76a6211a89c08d1ab1f793d94a128e796559c
chiaweilin <lin.chiawe@husky.neu.edu>
2020-06-24 15:05:17 -0700

    finish Multiple Enviroments section

--- pro-mern-stack-2/api/scripts/trymongo.js
+++ pro-mern-stack-2/api/scripts/trymongo.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require('mongodb');
-const url = 'mongodb://localhost/issuetracker';
+const url = process.env.DB_URL || 'mongodb://localhost/issuetracker';
 
 function testWithCallbacks(callback){
     console.log('\n--- testWithCallbacks ---');
@@ -11,7 +11,7 @@ function testWithCallbacks(callback){
             callback(err);
             return;
         }
-        console.log('Connected to MongoDB');
+        console.log('Connected to MongoDB URL', url);
 
         const db = client.db();
         const collection = db.collection('employees');
@@ -47,7 +47,7 @@ async function testWithAsync() {
     const client = new MongoClient(url, { useNewUrlParser: true });
     try {
       await client.connect();
-      console.log('Connected to MongoDB');
+      console.log('Connected to MongoDB URL', url);
       const db = client.db();
       const collection = db.collection('employees');
       const employee = { id: 2, name: 'B. Async', age: 16 };
