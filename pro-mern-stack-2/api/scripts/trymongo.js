722fb353be1543343de1cd335a170ec1c6bdd230
chiaweilin <lin.chiawe@husky.neu.edu>
2020-06-26 15:47:39 -0700

    finish ESLint (Back-end) section

--- pro-mern-stack-2/api/scripts/trymongo.js
+++ pro-mern-stack-2/api/scripts/trymongo.js
@@ -1,72 +1,73 @@
 const { MongoClient } = require('mongodb');
+
 const url = process.env.DB_URL || 'mongodb://localhost/issuetracker';
 
-function testWithCallbacks(callback){
-    console.log('\n--- testWithCallbacks ---');
+function testWithCallbacks(callback) {
+  console.log('\n--- testWithCallbacks ---');
 
 
-    const client = new MongoClient(url, {useNewUrlParser: true});
-    client.connect((err, client) => {
-        if (err){
+  const client = new MongoClient(url, { useNewUrlParser: true });
+  client.connect((connErr) => {
+    if (connErr) {
+      callback(connErr);
+      return;
+    }
+    console.log('Connected to MongoDB URL', url);
+
+    const db = client.db();
+    const collection = db.collection('employees');
+
+    const employee = { id: 1, name: 'A. Callback', age: 23 };
+
+    collection.insertOne(employee, (insertErr, result) => {
+      if (insertErr) {
+        client.close();
+        callback(insertErr);
+        return;
+      }
+      console.log('Result of insert:\n', result.insertedId);
+
+      collection.find({ _id: result.insertedId })
+        .toArray((err, docs) => {
+          if (err) {
+            client.close();
             callback(err);
             return;
-        }
-        console.log('Connected to MongoDB URL', url);
-
-        const db = client.db();
-        const collection = db.collection('employees');
-    
-        const employee = { id: 1, name: 'A. Callback', age: 23 };
-    
-        collection.insertOne(employee, function(err, result) {
-            if(err) {
-                client.close();
-                callback(err);
-                return;
-            }
-            console.log('Result of insert:\n', result.insertedId);
-
-            collection.find({ _id: result.insertedId})
-              .toArray(function(err, docs) {
-                if(err) {
-                    client.close();
-                    callback(err);
-                    return;
-                }
-                console.log('Result of find:\n', docs);
-                client.close();
-                callback(err);
-            });
+          }
+          console.log('Result of find:\n', docs);
+          client.close();
+          callback(err);
         });
     });
+  });
 }
 
 
 async function testWithAsync() {
-    console.log('\n--- testWithAsync ---');
-    const client = new MongoClient(url, { useNewUrlParser: true });
-    try {
-      await client.connect();
-      console.log('Connected to MongoDB URL', url);
-      const db = client.db();
-      const collection = db.collection('employees');
-      const employee = { id: 2, name: 'B. Async', age: 16 };
-      const result = await collection.insertOne(employee);
-      console.log('Result of insert:\n', result.insertedId);
-      const docs = await collection.find({ _id: result.insertedId })
-        .toArray();
-      console.log('Result of find:\n', docs);
-    } catch(err) {
-      console.log(err);
-    } finally {
-      client.close();
-    }
+  console.log('\n--- testWithAsync ---');
+  const client = new MongoClient(url, { useNewUrlParser: true });
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB URL', url);
+    const db = client.db();
+    const collection = db.collection('employees');
+    const employee = { id: 2, name: 'B. Async', age: 16 };
+    const result = await collection.insertOne(employee);
+    console.log('Result of insert:\n', result.insertedId);
+    const docs = await collection.find({ _id: result.insertedId })
+      .toArray();
+    console.log('Result of find:\n', docs);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    client.close();
+  }
 }
 
 
-testWithCallbacks(function(err) {
-    if (err) {
-      console.log(err);
-    }
-    testWithAsync();
+testWithCallbacks((err) => {
+  if (err) {
+    console.log(err);
+  }
+  testWithAsync();
 });
