94cedd1d41de6d55a9f498a92742e77cd48fccee
chiaweilin <lin.chiawe@husky.neu.edu>
2020-06-26 16:09:52 -0700

    finish ESLint for the Front-End section

--- pro-mern-stack-2/ui/src/App.jsx
+++ pro-mern-stack-2/ui/src/App.jsx
@@ -1,3 +1,10 @@
+/* eslint "react/react-in-jsx-scope": "off" */
+/* globals React ReactDOM */
+/* eslint "react/jsx-no-undef": "off" */
+/* eslint "react/no-multi-comp": "off" */
+/* eslint "no-alert": "off" */
+
+// eslint-disable-next-line react/prefer-stateless-function
 class IssueFilter extends React.Component {
   render() {
     return (
@@ -13,8 +20,7 @@ function jsonDateReviver(key, value) {
   return value;
 }
 
-function IssueRow(props) {
-  const issue = props.issue;
+function IssueRow({ issue }) {
   return (
     <tr>
       <td>{issue.id}</td>
@@ -28,10 +34,10 @@ function IssueRow(props) {
   );
 }
 
-function IssueTable(props) {
-  const issueRows = props.issues.map(issue =>
+function IssueTable({ issues }) {
+  const issueRows = issues.map(issue => (
     <IssueRow key={issue.id} issue={issue} />
-  );
+  ));
 
   return (
     <table className="bordered-table">
@@ -63,11 +69,14 @@ class IssueAdd extends React.Component {
     e.preventDefault();
     const form = document.forms.issueAdd;
     const issue = {
-      owner: form.owner.value, title: form.title.value,
-      due: new Date(new Date().getTime() + 1000*60*60*24*10),
-    }
-    this.props.createIssue(issue);
-    form.owner.value = ""; form.title.value = "";
+      owner: form.owner.value,
+      title: form.title.value,
+      due: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 10),
+    };
+    const { createIssue } = this.props;
+    createIssue(issue);
+    form.owner.value = '';
+    form.title.value = '';
   }
 
   render() {
@@ -75,7 +84,7 @@ class IssueAdd extends React.Component {
       <form name="issueAdd" onSubmit={this.handleSubmit}>
         <input type="text" name="owner" placeholder="Owner" />
         <input type="text" name="title" placeholder="Title" />
-        <button>Add</button>
+        <button type="submit">Add</button>
       </form>
     );
   }
@@ -85,14 +94,14 @@ async function graphQLFetch(query, variables = {}) {
   try {
     const response = await fetch(window.ENV.UI_API_ENDPOINT, {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json'},
-      body: JSON.stringify({ query, variables })
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ query, variables }),
     });
     const body = await response.text();
     const result = JSON.parse(body, jsonDateReviver);
     if (result.errors) {
       const error = result.errors[0];
-      if (error.extensions.code == 'BAD_USER_INPUT') {
+      if (error.extensions.code === 'BAD_USER_INPUT') {
         const details = error.extensions.exception.errors.join('\n ');
         alert(`${error.message}:\n ${details}`);
       } else {
@@ -102,6 +111,7 @@ async function graphQLFetch(query, variables = {}) {
     return result.data;
   } catch (e) {
     alert(`Error in sending data to server: ${e.message}`);
+    return null;
   }
 }
 
@@ -125,8 +135,8 @@ class IssueList extends React.Component {
     }`;
     const data = await graphQLFetch(query);
     if (data) {
-          this.setState({ issues: data.issueList });
-        }
+      this.setState({ issues: data.issueList });
+    }
   }
 
   async createIssue(issue) {
@@ -143,12 +153,13 @@ class IssueList extends React.Component {
   }
 
   render() {
+    const { issues } = this.state;
     return (
       <React.Fragment>
         <h1>Issue Tracker</h1>
         <IssueFilter />
         <hr />
-        <IssueTable issues={this.state.issues} />
+        <IssueTable issues={issues} />
         <hr />
         <IssueAdd createIssue={this.createIssue} />
       </React.Fragment>
