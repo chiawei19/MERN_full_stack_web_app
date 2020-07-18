c48cef90d02a62466619c7df70ecf52de1654f63
chiaweilin <lin.chiawe@husky.neu.edu>
2020-07-18 14:53:09 -0700

    finish New Directory Structure section

--- pro-mern-stack-2/api/scripts/generate_data.mongo.js
+++ pro-mern-stack-2/api/scripts/generate_data.mongo.js
@@ -4,11 +4,9 @@ const owners = ['Ravan', 'Eddie', 'Pieta', 'Parvati', 'Victor'];
 const statuses = ['New', 'Assigned', 'Fixed', 'Closed'];
 const initialCount = db.issues.count();
 for (let i = 0; i < 100; i += 1) {
-  const randomCreatedDate =
-    new Date() - Math.floor(Math.random() * 60) * 1000 * 60 * 60 * 24;
+  const randomCreatedDate = new Date() - Math.floor(Math.random() * 60) * 1000 * 60 * 60 * 24;
   const created = new Date(randomCreatedDate);
-  const randomDueDate =
-    new Date() - Math.floor(Math.random() * 60) * 1000 * 60 * 60 * 24;
+  const randomDueDate = new Date() - Math.floor(Math.random() * 60) * 1000 * 60 * 60 * 24;
   const due = new Date(randomDueDate);
   const owner = owners[Math.floor(Math.random() * 5)];
   const status = statuses[Math.floor(Math.random() * 4)];
