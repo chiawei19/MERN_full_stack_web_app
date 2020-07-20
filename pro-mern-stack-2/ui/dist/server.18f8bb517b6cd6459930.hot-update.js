2680cc34c33f461ec66eadb9a4c82b1aada7e796
chiaweilin <lin.chiawe@husky.neu.edu>
2020-07-20 01:18:28 -0700

    finish Data from API section

---
+++ pro-mern-stack-2/ui/dist/server.18f8bb517b6cd6459930.hot-update.js
@@ -0,0 +1,118 @@
+exports.id = "server";
+exports.modules = {
+
+/***/ "./server/render.jsx":
+false,
+
+/***/ "./server/template.js":
+false,
+
+/***/ "./server/uiserver.js":
+/*!****************************!*\
+  !*** ./server/uiserver.js ***!
+  \****************************/
+/*! no exports provided */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: /Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/server/uiserver.js: Unexpected token (36:4)\n\n\u001b[0m \u001b[90m 34 | \u001b[39m}\u001b[0m\n\u001b[0m \u001b[90m 35 | \u001b[39m\u001b[0m\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 36 | \u001b[39m\u001b[36mif\u001b[39m ()\u001b[0m\n\u001b[0m \u001b[90m    | \u001b[39m    \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 37 | \u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 38 | \u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 39 | \u001b[39mapp\u001b[33m.\u001b[39mget(\u001b[32m'/env.js'\u001b[39m\u001b[33m,\u001b[39m (req\u001b[33m,\u001b[39m res) \u001b[33m=>\u001b[39m {\u001b[0m\n    at Object._raise (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:754:17)\n    at Object.raiseWithData (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:747:17)\n    at Object.raise (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:741:17)\n    at Object.unexpected (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:8844:16)\n    at Object.parseExprAtom (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:10167:20)\n    at Object.parseExprAtom (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:4658:20)\n    at Object.parseExprSubscripts (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:9693:23)\n    at Object.parseMaybeUnary (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:9673:21)\n    at Object.parseExprOps (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:9543:23)\n    at Object.parseMaybeConditional (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:9516:23)\n    at Object.parseMaybeAssign (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:9471:21)\n    at Object.parseExpression (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:9423:23)\n    at Object.parseHeaderExpression (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:11462:22)\n    at Object.parseIfStatement (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:11544:22)\n    at Object.parseStatementContent (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:11238:21)\n    at Object.parseStatement (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:11193:17)\n    at Object.parseBlockOrModuleBlockBody (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:11768:25)\n    at Object.parseBlockBody (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:11754:10)\n    at Object.parseTopLevel (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:11124:10)\n    at Object.parse (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:12826:10)\n    at parse (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:12879:38)\n    at parser (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/core/lib/parser/index.js:54:34)\n    at parser.next (<anonymous>)\n    at normalizeFile (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/core/lib/transformation/normalize-file.js:93:38)\n    at normalizeFile.next (<anonymous>)\n    at run (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/core/lib/transformation/index.js:31:50)\n    at run.next (<anonymous>)\n    at Function.transform (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/core/lib/transform.js:27:41)\n    at transform.next (<anonymous>)\n    at step (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/gensync/index.js:254:32)");
+
+/***/ }),
+
+/***/ "./src/About.jsx":
+false,
+
+/***/ "./src/Contents.jsx":
+false,
+
+/***/ "./src/DateInput.jsx":
+false,
+
+/***/ "./src/IssueAddNavItem.jsx":
+false,
+
+/***/ "./src/IssueDetail.jsx":
+false,
+
+/***/ "./src/IssueEdit.jsx":
+false,
+
+/***/ "./src/IssueFilter.jsx":
+false,
+
+/***/ "./src/IssueList.jsx":
+false,
+
+/***/ "./src/IssueReport.jsx":
+false,
+
+/***/ "./src/IssueTable.jsx":
+false,
+
+/***/ "./src/NumInput.jsx":
+false,
+
+/***/ "./src/Page.jsx":
+false,
+
+/***/ "./src/TextInput.jsx":
+false,
+
+/***/ "./src/Toast.jsx":
+false,
+
+/***/ "./src/graphQLFetch.js":
+false,
+
+/***/ "./src/withToast.jsx":
+false,
+
+/***/ "./webpack.config.js":
+false,
+
+/***/ "dotenv":
+false,
+
+/***/ "express":
+false,
+
+/***/ "http-proxy-middleware":
+false,
+
+/***/ "path":
+false,
+
+/***/ "react":
+false,
+
+/***/ "react-bootstrap":
+false,
+
+/***/ "react-dom/server":
+false,
+
+/***/ "react-router-bootstrap":
+false,
+
+/***/ "react-router-dom":
+false,
+
+/***/ "source-map-support":
+false,
+
+/***/ "url-search-params":
+false,
+
+/***/ "webpack":
+false,
+
+/***/ "webpack-dev-middleware":
+false,
+
+/***/ "webpack-hot-middleware":
+false,
+
+/***/ "webpack-node-externals":
+false
+
+};
+//# sourceMappingURL=server.18f8bb517b6cd6459930.hot-update.js.map
