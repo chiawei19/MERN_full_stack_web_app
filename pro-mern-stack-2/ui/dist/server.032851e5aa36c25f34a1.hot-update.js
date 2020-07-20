8af86fc34b8906769a4fde5d858f8eab0ff78052
chiaweilin <lin.chiawe@husky.neu.edu>
2020-07-20 02:04:03 -0700

    finish Generated Routetes section

---
+++ pro-mern-stack-2/ui/dist/server.032851e5aa36c25f34a1.hot-update.js
@@ -0,0 +1,97 @@
+exports.id = "server";
+exports.modules = {
+
+/***/ "./server/render.jsx":
+/*!***************************!*\
+  !*** ./server/render.jsx ***!
+  \***************************/
+/*! exports provided: default */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: /Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/server/render.jsx: Unexpected token (11:6)\n\n\u001b[0m \u001b[90m  9 | \u001b[39m\u001b[36mimport\u001b[39m \u001b[0m\n\u001b[0m \u001b[90m 10 | \u001b[39m\u001b[0m\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 11 | \u001b[39masync \u001b[36mfunction\u001b[39m render(req\u001b[33m,\u001b[39m res) {\u001b[0m\n\u001b[0m \u001b[90m    | \u001b[39m      \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 12 | \u001b[39m  \u001b[36mconst\u001b[39m initialData \u001b[33m=\u001b[39m \u001b[33mAbout\u001b[39m\u001b[33m.\u001b[39mfetchData()\u001b[33m;\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 13 | \u001b[39m  store\u001b[33m.\u001b[39minitialData \u001b[33m=\u001b[39m initialData\u001b[33m;\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 14 | \u001b[39m  \u001b[36mconst\u001b[39m element \u001b[33m=\u001b[39m (\u001b[0m\n    at Object._raise (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:754:17)\n    at Object.raiseWithData (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:747:17)\n    at Object.raise (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:741:17)\n    at Object.unexpected (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:8844:16)\n    at Object.expectContextual (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:8810:41)\n    at Object.parseImport (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:12585:12)\n    at Object.parseStatementContent (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:11291:27)\n    at Object.parseStatement (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:11193:17)\n    at Object.parseBlockOrModuleBlockBody (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:11768:25)\n    at Object.parseBlockBody (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:11754:10)\n    at Object.parseTopLevel (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:11124:10)\n    at Object.parse (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:12826:10)\n    at parse (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/parser/lib/index.js:12879:38)\n    at parser (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/core/lib/parser/index.js:54:34)\n    at parser.next (<anonymous>)\n    at normalizeFile (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/core/lib/transformation/normalize-file.js:93:38)\n    at normalizeFile.next (<anonymous>)\n    at run (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/core/lib/transformation/index.js:31:50)\n    at run.next (<anonymous>)\n    at Function.transform (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/@babel/core/lib/transform.js:27:41)\n    at transform.next (<anonymous>)\n    at step (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/gensync/index.js:254:32)\n    at gen.next (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/gensync/index.js:266:13)\n    at async.call.value (/Users/chia-wei/Documents/cs5610/MERN_full_stack_web_app/pro-mern-stack-2/ui/node_modules/gensync/index.js:216:11)");
+
+/***/ }),
+
+/***/ "./server/template.js":
+false,
+
+/***/ "./src/About.jsx":
+false,
+
+/***/ "./src/Contents.jsx":
+false,
+
+/***/ "./src/DateInput.jsx":
+false,
+
+/***/ "./src/IssueAddNavItem.jsx":
+false,
+
+/***/ "./src/IssueDetail.jsx":
+false,
+
+/***/ "./src/IssueEdit.jsx":
+false,
+
+/***/ "./src/IssueFilter.jsx":
+false,
+
+/***/ "./src/IssueList.jsx":
+false,
+
+/***/ "./src/IssueReport.jsx":
+false,
+
+/***/ "./src/IssueTable.jsx":
+false,
+
+/***/ "./src/NotFound.jsx":
+false,
+
+/***/ "./src/NumInput.jsx":
+false,
+
+/***/ "./src/Page.jsx":
+false,
+
+/***/ "./src/TextInput.jsx":
+false,
+
+/***/ "./src/Toast.jsx":
+false,
+
+/***/ "./src/graphQLFetch.js":
+false,
+
+/***/ "./src/routes.js":
+false,
+
+/***/ "./src/store.js":
+false,
+
+/***/ "./src/withToast.jsx":
+false,
+
+/***/ "isomorphic-fetch":
+false,
+
+/***/ "react":
+false,
+
+/***/ "react-bootstrap":
+false,
+
+/***/ "react-dom/server":
+false,
+
+/***/ "react-router-bootstrap":
+false,
+
+/***/ "react-router-dom":
+false,
+
+/***/ "url-search-params":
+false
+
+};
+//# sourceMappingURL=server.032851e5aa36c25f34a1.hot-update.js.map
---
+++ pro-mern-stack-2/ui/dist/server.0900ef3b30d0f683564a.hot-update.js
@@ -0,0 +1,45 @@
+exports.id = "server";
+exports.modules = {
+
+/***/ "./src/Contents.jsx":
+/*!**************************!*\
+  !*** ./src/Contents.jsx ***!
+  \**************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Contents; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-router-dom */ "react-router-dom");
+/* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_router_dom__WEBPACK_IMPORTED_MODULE_1__);
+
+
+function Contents() {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Switch"], null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Redirect"], {
+    exact: true,
+    from: "/",
+    to: "/issues"
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Route"], {
+    path: "/issues",
+    component: IssueList
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Route"], {
+    path: "/edit/:id",
+    component: IssueEdit
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Route"], {
+    path: "/report",
+    component: IssueReport
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Route"], {
+    path: "/about",
+    component: About
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Route"], {
+    component: NotFound
+  }));
+}
+
+/***/ })
+
+};
+//# sourceMappingURL=server.0900ef3b30d0f683564a.hot-update.js.map
